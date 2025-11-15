666387e16e5eab57e232d713b0f77e3009be68e0
culas <lucas.schnueriger@me.com>
2025-11-15 21:02:06 +0100

    add some highlighting and weekdays in calendar

--- src/app/calendar/calendar.ts
+++ src/app/calendar/calendar.ts
@@ -6,9 +6,14 @@ import {get_weeks_of_month} from '../helpers/get_weeks_of_month';
   imports: [],
   template: `
     <section>
+      @for (weekday of weekdays(); track weekday) {
+        <span>{{ weekday }}</span>
+      }
       @for (week of weeks(); track week[0].getTime()) {
         @for (day of week; track day.getUTCDate()) {
-          <button (click)="date.set(day)">
+          <button [class.current-month]="active_month() === day.getUTCMonth()"
+                  [class.active]="date().getTime() === day.getTime()"
+                  (click)="date.set(day)">
             {{ day.getUTCDate() }}
           </button>
         }
@@ -21,4 +26,7 @@ export class Calendar {
   public readonly date = model.required<Date>();
 
   protected readonly weeks = computed(() => get_weeks_of_month(this.date()));
+  protected readonly active_month = computed(() => this.date().getUTCMonth());
+
+  protected readonly weekdays = computed(() => this.weeks()[0].map(day => day.toLocaleDateString(undefined, {weekday: 'short'})))
 }
