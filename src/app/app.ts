1c3e6f0c6a8e86dfac3724579cec11b8b72941e3
culas <lucas.schnueriger@me.com>
2025-11-17 10:06:10 +0100

    add primitive month selector, semi working

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,30 @@
-import {Component, signal} from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {Calendar} from './calendar/calendar';
 import {Controls} from './controls/controls';
+import {MonthSelector} from './month-selector/month-selector';
 
 @Component({
   selector: 'app-root',
   imports: [
     FormsModule,
     Calendar,
-    Controls
+    Controls,
+    MonthSelector
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly date = signal(new Date());
+  protected readonly date = signal(new Date()); // timezone might offset to wrong day
+  protected readonly mode = signal<'calendar' | 'month'>('calendar');
+  protected readonly iso_date = computed(() => this.date().toISOString().split('T')[0]);
 
   protected change_month(offset: number) {
     this.date.update(date => {
       date.setUTCMonth(date.getUTCMonth() + offset);
+      // todo: handle overflows (e.g. on 31st change to feb)
+      console.log(date.getUTCMonth());
       return new Date(date);
     });
   }
@@ -41,5 +47,18 @@ export class App {
 
   protected set_to_today() {
     this.date.set(new Date());
+  }
+
+  protected set_month(month: Date) {
+    this.date.update(date => {
+      date.setUTCMonth(month.getUTCMonth());
+      // todo: handle overflows (e.g. on 31st change to feb)
+      return new Date(date);
+    });
+    this.mode.set('calendar');
+  }
+
+  protected select_month() {
+    this.mode.update(mode => mode === 'month' ? 'calendar' : 'month');
   }
 }
---
+++ src/app/month-selector/month-selector.ts
@@ -0,0 +1,41 @@
+import {Component, input, output} from '@angular/core';
+
+@Component({
+  selector: 'app-month-selector',
+  template: `
+    <section>
+      @for (month of months; track month.getTime()) {
+        <button
+          (click)="set_month.emit(month)"
+          [class.active]="month.getUTCMonth() === date().getUTCMonth()"
+        >{{ month.toLocaleDateString(undefined, {month: 'long'}) }}
+        </button>
+      }
+    </section>
+  `,
+  styles: [`
+    section {
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      gap: .5rem;
+      padding: .5rem;
+      border: 1px solid lightgrey;
+    }
+
+    .active {
+      font-weight: bold;
+    }
+  `]
+})
+export class MonthSelector {
+  public readonly date = input.required<Date>();
+  public readonly set_month = output<Date>();
+
+  protected readonly months: Date[] = new Array(12)
+    .fill(this.date())
+    .map((date, i) => {
+      const new_date = new Date(date);
+      new_date.setUTCMonth(i);
+      return new_date;
+    });
+}
